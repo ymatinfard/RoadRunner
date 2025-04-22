31e2e8f5987c0dc5fb7f187134443b19466c772f
Yousef <matinfard.y@gmail.com>
2025-04-22 11:57:21 +0200

    Update QuestScreen to display runner and driver icons

--- app/src/main/java/com/matin/roadrunner/feature/mainqeust/QuestScreen.kt
+++ app/src/main/java/com/matin/roadrunner/feature/mainqeust/QuestScreen.kt
@@ -1,12 +1,14 @@
 package com.matin.roadrunner.feature.mainqeust
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -15,11 +17,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.matin.roadrunner.R
 import com.matin.roadrunner.core.designsystem.RoadRunnerTheme
 import com.matin.roadrunner.feature.mainqeust.model.DriverUiModel
 import com.matin.roadrunner.feature.mainqeust.model.GroundCellUiModel
@@ -39,7 +44,6 @@ fun QuestScreenContent(
         val cellModifier =
             Modifier
                 .aspectRatio(1f)
-                .background(color = MaterialTheme.colorScheme.primary)
                 .border(BorderStroke(1.dp, color = MaterialTheme.colorScheme.tertiary))
 
         LazyVerticalGrid(
@@ -65,10 +69,20 @@ fun GroundCell(
         modifier =
             modifier
                 .clickable { onCellClick(cell) },
+        contentAlignment = Alignment.Center,
     ) {
         when {
-            cell.hasRunner -> Text("R")
-            cell.driver != null -> Text("D")
+            cell.hasRunner -> Image(
+                painter = painterResource(id = R.drawable.ic_person),
+                modifier = Modifier.size(24.dp),
+                contentDescription = "RoadRunner",
+            )
+
+            cell.driver != null -> Image(
+                painter = painterResource(id = R.drawable.ic_taxi),
+                modifier = Modifier.size(24.dp),
+                contentDescription = "Taxi",
+            )
         }
     }
 }
@@ -82,8 +96,9 @@ fun QuestScreenPreview() {
 }
 
 val fakeGroundCells =
-    List(50) { index ->
+    List(100){ index ->
         GroundCellUiModel(
+            hasRunner = index == 55,
             driver =
                 if (index % 10 == 0) {
                     DriverUiModel("1", "Driver 1", 1, 2, 3)
